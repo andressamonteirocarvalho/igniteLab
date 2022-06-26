2f3178ed9ad4e11235adee322305c7da430222bc
Andressa Monteiro <andressa18monteiro@gmail.com>
2022-06-25 22:54:50 -0300

    Aula1 - O início da especialização em ReactJs

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.tsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+//Dentro do content, eu estou informando quais os
+//arquivos receberão o Taiwand css
